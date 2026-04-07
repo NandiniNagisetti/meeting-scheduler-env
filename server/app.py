7e3cac4a2c8c74a3498084fab18acab39d21ff1e
NandiniNagisetti <163736023+NandiniNagisetti@users.noreply.github.com>
2026-04-07 23:02:59 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -42,12 +42,10 @@ def predict(req: Request):
     }
 
 
-# ✅ REQUIRED MAIN FUNCTION
 def main():
     import uvicorn
     uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
 
 
-# ✅ REQUIRED ENTRY POINT
 if __name__ == "__main__":
     main()
